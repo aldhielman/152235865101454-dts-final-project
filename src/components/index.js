a93bd38b13cc985c3487c8b40785481f590c2243
aldhielman <rivaldithoriq@gmail.com>
2022-07-26 16:23:31 +0700

    add importer for page and component

---
+++ src/components/index.js
@@ -0,0 +1,21 @@
+import Navbar from "./NavBar/NavBar";
+import Map from "./Map/Map";
+import LineGraph from "./LineGraph/LineGraph";
+import InfoBox from "./InfoBox/InfoBox";
+import Table from "./Table/Table";
+import MapMarker from "./MapMarker/MapMarker";
+import SetMapView from "./SetMapView/SetMapView";
+import SocialLogin from "./SocialLogin/SocialLogin";
+import Copyright from "./Copyright/Copyright";
+
+export {
+  Navbar,
+  Map,
+  LineGraph,
+  InfoBox,
+  Table,
+  MapMarker,
+  SetMapView,
+  SocialLogin,
+  Copyright,
+};
---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+import Home from "./Home/Home";
+import Detail from "./Detail/Detail";
+import Register from "./Register/Register";
+import Login from "./Login/Login";
+import NotFound404 from "./NotFound404/NotFound404";
+
+export { Home, Detail, Register, Login, NotFound404 };
