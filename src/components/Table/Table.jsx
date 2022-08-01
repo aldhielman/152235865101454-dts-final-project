876b3bdc703e2e4b0fd24b07c5a7fe5a5e14ca28
aldhielman <rivaldithoriq@gmail.com>
2022-08-01 16:15:17 +0700

    using table mui

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -2,6 +2,15 @@ import React from "react";
 import "./Table.css";
 import numeral from "numeral";
 import { Link, useNavigate } from "react-router-dom";
+import {
+  Paper,
+  Table as MuiTable,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from "@mui/material";
 
 function Table({ countries }) {
   const navigate = useNavigate();
@@ -10,20 +19,42 @@ function Table({ countries }) {
   };
   console.log(countries);
   return (
-    <div className="table">
-      {countries.map((country) => (
-        <tr
-          key={country.country}
-          onClick={(e) => handleOnclick(e, country.countryInfo.iso2)}
-        >
-          <td>{country.country}</td>
-          <td>
-            <strong>{numeral(country.cases).format(0)}</strong>
-          </td>
-        </tr>
-      ))}
-    </div>
+    <Paper sx={{ width: "100%", overflow: "hidden" }}>
+      <TableContainer sx={{ maxHeight: 440 }}>
+        <MuiTable stickyHeader aria-label="sticky table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Country</TableCell>
+              <TableCell>Cases</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {countries.map((country) => (
+              <TableRow>
+                <TableCell>{country.country}</TableCell>
+                <TableCell>{country.country}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </MuiTable>
+      </TableContainer>
+    </Paper>
   );
+  // return (
+  //   <div className="table">
+  //     {countries.map((country) => (
+  //       <tr
+  //         key={country.country}
+  //         onClick={(e) => handleOnclick(e, country.countryInfo.iso2)}
+  //       >
+  //         <td>{country.country}</td>
+  //         <td>
+  //           <strong>{numeral(country.cases).format(0)}</strong>
+  //         </td>
+  //       </tr>
+  //     ))}
+  //   </div>
+  // );
 }
 
 export default Table;
